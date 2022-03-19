e56e4cb3cccec22db48c9f197d7d65a159dc429e
knight37x <adrian.bauske@gmail.com>
2022-03-19 12:24:52 +0100

    understanding important parts of can message

--- main/can.c
+++ main/can.c
@@ -12,7 +12,7 @@
 static const char *CAN_TAG = "CAN";
 
 static const twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
-static const twai_timing_config_t t_config = TWAI_TIMING_CONFIG_25KBITS();
+static const twai_timing_config_t t_config = TWAI_TIMING_CONFIG_250KBITS();
 //Set TX queue length to 0 due to listen only mode
 static const twai_general_config_t g_config = {.mode = TWAI_MODE_LISTEN_ONLY,
     .tx_io = TX_GPIO_NUM, .rx_io = RX_GPIO_NUM,
@@ -25,7 +25,7 @@ static CAN_MsgHandler msgHandler;
 
 static void canReceiveTask(void *arg) {
   while (1) {
-    ESP_LOGI(CAN_TAG, "Trying to receive next CAN message");
+//    ESP_LOGI(CAN_TAG, "Trying to receive next CAN message");
     twai_message_t rx_msg;
     esp_err_t err = twai_receive(&rx_msg, portMAX_DELAY);
     if(err == ESP_ERR_TIMEOUT) {
--- main/esp_hid_host_main.c
+++ main/esp_hid_host_main.c
@@ -17,13 +17,25 @@ static void canHandler(twai_message_t* msg) {
   uint8_t data[12];
   memcpy(data, &msg->identifier, sizeof(msg->identifier));
   memcpy(data + sizeof(msg->identifier), msg->data, msg->data_length_code);
-  printf("CAN: Received message: %08X         %02X %02X %02X %02X %02X %02X %02X %02X", msg->identifier, data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11]);
-  bleSetData(data, msg->data_length_code + sizeof(msg->identifier));
+  switch(msg->identifier) {
+    case 0x201: // Laenge 5 Bytes nur!   Byte 0-1: geschwindigkeit: wert / 100 = km/h; Byte 4: 00 = keine unterstützung, 20=Stufe 1, 40=Stufe 2, 60=Stufe 3, 80=Stufe 4, 02=Licht an;rest konstant 0er
+    case 0x203:
+    case 0x300: // Laenge 4;   Byte 3: Fahrrad an: 5A, aus: AA; byte 0: 00=keine Unterstützung; 04=Vollgas; Byte 2: 00=Licht aus, 64=licht an
+      return;
+    // nach anschalten:
+    case 0x200: // ändert sich fast gar nicht nur Byte 6 (das 7.) zyklisch von 0 bis 2; erstes Byte immer 100, andere immer 0
+    case 0x665: // konstant: 00 00 00 53 4C 18 00 00
+    case 0x202: // Byte 4-7: Laufleistung in metern! Little Endian ; Byte 0 immer 9A, Byte 2+3 immer 0; Byte 1 wechselt wild zwischen 12 bis 15
+    case 0x666: // konstant: 52 06 01 00 52 01 38 52
+      return;
+  }
+      printf("CAN: Received message: %08X     %d    %02X %02X %02X %02X %02X %02X %02X %02X\n", msg->identifier, msg->data_length_code, data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11]);
+//  bleSetData(data, msg->data_length_code + sizeof(msg->identifier));
 }
 
 void app_main(void)
 {
   ledInit(2);
-//  canInit(canHandler);
-  bleInit();
+  canInit(canHandler);
+//  bleInit();
 }
