e447633ac45cec912d9123a67c582fb2af6b43fd
knight37x <adrian.bauske@gmail.com>
2023-05-07 13:26:32 +0200

    working

--- src/multicore_runner_queue.c
+++ src/multicore_runner_queue.c
@@ -6,10 +6,14 @@
 
 #include <stdio.h>
 #include <intctrl.h>
+#include <hardware/watchdog.h>
 #include "pico/stdlib.h"
 #include "pico/util/queue.h"
 #include "pico/multicore.h"
 #include "can2040.h"
+
+#define CAN_TIMEOUT_US 500000
+#define REAL_SPEED_TIMEOUT_MS 10000
 
 typedef struct can2040 CANHandle;
 typedef struct can2040_msg CANMsg;
@@ -17,17 +21,25 @@ typedef struct can2040_msg CANMsg;
 static CANHandle can0;
 static CANHandle can1;
 
+static absolute_time_t can0TxTime;
+static absolute_time_t can0RxTime;
+static absolute_time_t can1TxTime;
+static absolute_time_t can1RxTime;
+
+static absolute_time_t realSpeedMsgTime;
+static uint16_t realSpeed;
+
 static void can2040_cb0(CANHandle *cd, uint32_t notify, CANMsg *msg) {
   switch (notify) {
     case CAN2040_NOTIFY_RX:
-      printf("can0 received a message. sending it back!");
-      can2040_transmit(&can0, msg); // just send it back
+      can0RxTime = get_absolute_time();
+//      printf("Received CAN0\n");
+      can2040_transmit(&can1, msg);
       break;
     case CAN2040_NOTIFY_TX:
-      printf("can0 successfully send message\n");
+      can0TxTime = get_absolute_time();
       break;
     case CAN2040_NOTIFY_ERROR:
-      printf("can0 error\n");
       break;
   }
 }
@@ -35,37 +47,46 @@ static void can2040_cb0(CANHandle *cd, uint32_t notify, CANMsg *msg) {
 static void can2040_cb1(CANHandle *cd, uint32_t notify, CANMsg *msg) {
   switch (notify) {
     case CAN2040_NOTIFY_RX: {
-      uint8_t* data = msg->data;
-      printf("Received message: %08X     %d    %02X %02X %02X %02X %02X %02X %02X %02X\n", msg->id, msg->dlc, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]);
+      can1RxTime = get_absolute_time();
+//      printf("Received CAN1\n");
+      if(msg->id == 0x201) { // motor speed msg
+        if(!time_reached(delayed_by_ms(realSpeedMsgTime, REAL_SPEED_TIMEOUT_MS))) {
+          // no timeout
+          *((uint16_t*) msg->data) = realSpeed;
+        }
+        can2040_transmit(&can0, msg);
+      } else if(msg->id == 0x737) { // real speed msg
+        realSpeedMsgTime = get_absolute_time();
+        realSpeed = *((uint16_t*) msg->data);
+      } else {
+        can2040_transmit(&can0, msg);
+      }
       break;
     }
     case CAN2040_NOTIFY_TX:
-      printf("can1 successfully send message\n");
+      can1TxTime = get_absolute_time();
       break;
     case CAN2040_NOTIFY_ERROR:
-      printf("can1 error\n");
       break;
   }
 }
 
 static void PIO0_IRQHandler(void) {
   can2040_pio_irq_handler(&can0);
-  printf("PIO0_IRQHandler called\n");
 }
 
 static void PIO1_IRQHandler(void) {
   can2040_pio_irq_handler(&can1);
-  printf("PIO1_IRQHandler called\n");
 }
 
 void canbus_setup(uint32_t pio_num, uint32_t gpio_rx, uint32_t gpio_tx) {
-  uint32_t sys_clock = 125000000, bitrate = 500;
+  uint32_t sys_clock = 125000000, bitrate = 250000;
 
   CANHandle* canX = pio_num == 0 ? &can0 : &can1;
 
   // Setup canbus
-  can2040_setup(&can0, pio_num);
-  can2040_callback_config(&can0, pio_num == 0 ? can2040_cb0 : can2040_cb1);
+  can2040_setup(canX, pio_num == 0 ? 0 : 1);
+  can2040_callback_config(canX, pio_num == 0 ? can2040_cb0 : can2040_cb1);
 
   // Enable irqs
   uint irqn = pio_num == 0 ? PIO0_IRQ_0 : PIO1_IRQ_0;
@@ -74,57 +95,82 @@ void canbus_setup(uint32_t pio_num, uint32_t gpio_rx, uint32_t gpio_tx) {
   irq_set_enabled(irqn, true);
 
   // Start canbus
-  can2040_start(&can0, sys_clock, bitrate, gpio_rx, gpio_tx);
+  can2040_start(canX, sys_clock, bitrate, gpio_rx, gpio_tx);
+}
+
+void wd_cond_trigger() {
+  absolute_time_t now = get_absolute_time();
+  if(absolute_time_diff_us(can0TxTime, now) < CAN_TIMEOUT_US &&
+      absolute_time_diff_us(can0RxTime, now) < CAN_TIMEOUT_US &&
+      absolute_time_diff_us(can1TxTime, now) < CAN_TIMEOUT_US &&
+      absolute_time_diff_us(can1RxTime, now) < CAN_TIMEOUT_US) {
+    watchdog_update();
+  }
 }
 
 int main() {
   stdio_init_all();
 
-  sleep_ms(10000);
-  printf("Startup delay over\n");
+//  sleep_ms(10000);
 
-  const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-  gpio_init(LED_PIN);
-  gpio_set_dir(LED_PIN, GPIO_OUT);
-  uint ledStatus = 0;
+  watchdog_enable(0x7fffff, 1);
+  watchdog_update();
+
+//  printf("Startup delay over\n");
+
+//  const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+//  gpio_init(LED_PIN);
+//  gpio_set_dir(LED_PIN, GPIO_OUT);
+//  uint ledStatus = 0;
 
   printf("Starting Initialization of CAN\n");
-  canbus_setup(0, 4, 5);
+  canbus_setup(0, 13, 14);
   printf("Initialized CAN0\n");
-//  canbus_setup(1, 2, 5);
+  canbus_setup(1, 8, 10);
   printf("Initialized CAN1\n");
 
+  can0TxTime = make_timeout_time_ms(CAN_TIMEOUT_US / 1000 * 30);
+  can0RxTime = can0TxTime;
+  can1TxTime = can0TxTime;
+  can1RxTime = can0TxTime;
 
-  sleep_ms(1000);
-//  gpio_set_pulls(2, 0, 0);
 
-
-//  while (true) {
-//    gpio_put(LED_PIN, 1);
-//    sleep_ms(250);
-//    gpio_put(LED_PIN, 0);
-//    sleep_ms(250);
-//  }
+  wd_cond_trigger();
+//  sleep_ms(1000);
+//  wd_cond_trigger();
 
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "EndlessLoop"
+
+  absolute_time_t lastPrintTime = get_absolute_time();
   while (true) {
-    CANMsg msg;
-    msg.dlc = 8;
-    msg.id = 34;
-    msg.data[0] = 11;
-    msg.data[1] = 22;
-    msg.data[2] = 33;
-    msg.data[3] = 44;
-    msg.data[4] = 55;
-    msg.data[5] = 66;
-    msg.data[6] = 77;
-    msg.data[7] = 88;
-    int res = can2040_transmit(&can0, &msg);
-    printf("Sending! returned: %d\n", res);
-    sleep_ms(5000);
-    gpio_put(LED_PIN, ledStatus);
-    ledStatus = !ledStatus;
+    wd_cond_trigger();
+//    CANMsg msg;
+//    msg.dlc = 8;
+//    msg.id = 34 + ledStatus;
+//    msg.data[0] = 11;
+//    msg.data[1] = 22;
+//    msg.data[2] = 33;
+//    msg.data[3] = 44;
+//    msg.data[4] = 55;
+//    msg.data[5] = 66;
+//    msg.data[6] = 77;
+//    msg.data[7] = 88;
+////    int res = can2040_transmit(&can1, &msg);
+//    int res = ledStatus == 0 ? can2040_transmit(&can0, &msg) : can2040_transmit(&can1, &msg);
+//    printf("Sending! returned: %d\n", res);
+    sleep_ms(100);
+//    gpio_put(LED_PIN, ledStatus);
+//    ledStatus = !ledStatus;
+
+    absolute_time_t now = get_absolute_time();
+    if(absolute_time_diff_us(lastPrintTime, now) >= 1000000) {
+      printf("ms since can0Tx: %ld can0Rx: %ld can1Tx: %ld can1Rx: %ld\n", absolute_time_diff_us(can0TxTime, now) / 1000,
+             absolute_time_diff_us(can0RxTime, now) / 1000,
+             absolute_time_diff_us(can1TxTime, now) / 1000,
+             absolute_time_diff_us(can1RxTime, now) / 1000);
+      lastPrintTime = now;
+    }
   }
 #pragma clang diagnostic pop
 }
